a59d678431ee38905e25e6c277055ba30caade10
Mon <mongamind@foxmail.com>
2016-11-15 01:46:25 +0800

    the back-up of DreamerShooter

---
+++ Assets/Scripts/Camara/CamaraFollowing.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamaraFollowing : MonoBehaviour {
+
+	Transform target;
+	Vector3 offset;
+	float smoothing = 5f;
+	void Start()
+	{
+		offset = transform.position - target.position;
+	}
+
+	void FixedUpate()
+	{
+		Vector3 pos = target.position + offset;
+		transform.position = Vector3.Lerp (transform.position, pos, smoothing * Time.deltaTime);
+
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 1f;
+	public float maxRaycastDistance = 100f;
+	private Animator anim;
+
+	private int floorLayermask;
+	private Rigidbody playerRigidbody;
+	void Awake()
+	{
+		playerRigidbody = GetComponent<Rigidbody> ();
+		anim = GetComponent<Animator> ();
+		floorLayermask = LayerMask.GetMask("Floor");
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		Move (h, v);
+		Rotate ();
+	}
+
+	void Move(float h,float v)
+	{
+		anim.SetBool ("isWalking", h != 0 || v != 0);
+		Vector3 toward = new Vector3 (h, 0f, v);
+		toward.Normalize ();
+		playerRigidbody.MovePosition (playerRigidbody.position +  toward * speed * Time.deltaTime);
+	}
+
+	void Rotate()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit raycastHit;
+
+		if (Physics.Raycast (ray,out raycastHit, maxRaycastDistance, floorLayermask)) {
+			Vector3 playerToMouse = raycastHit.point - transform.position;
+
+			playerToMouse.y = 0f;
+			Quaternion quaternion = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (quaternion);
+		}
+			
+	}
+}
